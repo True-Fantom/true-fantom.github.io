bfa34fc237e78c2b2b62777d8f1557ef1e09df44
Fantøm <116278271+Fantom-408@users.noreply.github.com>
2022-11-30 17:56:27 +0300

    Update Fantom.js

--- Fantom.js
+++ Fantom.js
@@ -92,7 +92,7 @@
             arguments: {
               A: {
                 type: Scratch.ArgumentType.STRING,
-                defaultValue: ''
+                defaultValue: null
               },
               B: {
                 type: Scratch.ArgumentType.STRING,
@@ -108,7 +108,7 @@
             arguments: {
               A: {
                 type: Scratch.ArgumentType.STRING,
-                defaultValue: ''
+                defaultValue: null
               },
               B: {
                 type: Scratch.ArgumentType.STRING,
