a41fd293ca128ab2505e361db724adbc360568a2
True Fantøm <116278271+TrueFantom@users.noreply.github.com>
2022-12-09 19:28:01 +0300

    Update ColorTest.js

--- ColorTest.js
+++ ColorTest.js
@@ -27,7 +27,7 @@
             text: '[bool]',
             arguments: {
               bool: {
-                type: Scratch.ArgumentType.STRING,
+                type: Scratch.ArgumentType.BOOLEAN,
               }
             }
           },
@@ -39,7 +39,6 @@
             arguments: {
               string: {
                 type: Scratch.ArgumentType.STRING,
-                defaultValue: ''
               }
             }
           },
