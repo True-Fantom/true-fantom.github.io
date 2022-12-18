819933204a5092cb5ad5c8d7571e73050eec77e1
True Fantøm <116278271+TrueFantom@users.noreply.github.com>
2022-12-18 10:30:26 +0300

    Update network.js

--- network.js
+++ network.js
@@ -188,7 +188,7 @@
         return fetch(String(URL), {
           method:'POST',
           headers: {
-            'Content-Type':CONTENT_TYPE
+            'Content-Type': CONTENT_TYPE
           },
           redirect: 'follow',
           body: BODY})
