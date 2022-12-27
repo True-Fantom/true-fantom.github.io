948843b651743b62bb1ae48691720dd7f0d17065
True Fantøm <116278271+TrueFantom@users.noreply.github.com>
2022-12-27 21:44:50 +0300

    Update network.js

--- network.js
+++ network.js
@@ -735,6 +735,20 @@
       }
     }
     
+    url_block({URL}) {
+      var url_test = /(^https?:\/\/)?[a-z0-9~_\-\.]+\.[a-z]{2,9}(\/|:|\?[!-~]*)?$/i;
+      return url_test.test(URL);
+    }
+    
+    website_block({URL}) {
+      return fetch(URL, {
+      method: 'GET',
+      headers: {},
+      redirect: 'follow'})
+      .then(res => res.ok)
+      .catch(err => false);
+    }
+    
     ping_block({SERVER}) {
       return cached_ping_web_socket(String(SERVER));
     }
