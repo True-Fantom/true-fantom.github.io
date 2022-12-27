fc12c102508bb7cc51dcff067c85a79c887fa058
True Fantøm <116278271+TrueFantom@users.noreply.github.com>
2022-12-27 19:12:05 +0300

    Update network.js

--- network.js
+++ network.js
@@ -106,12 +106,12 @@
         .catch(err => '');
       }
       else if (RESPONSE_TYPE === 3) {
-        return fetch(URL, {
+        fetch(URL, {
         method: METHOD,
         headers: {},
         redirect: 'follow'})
-        .then(res => res.ok)
-        .catch(err => '');
+        .then(res => {return res.ok})
+        .catch(err => {return ''});
       }
       else if (RESPONSE_TYPE === 4) {
         return fetch(URL, {
