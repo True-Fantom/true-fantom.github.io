cc80357b373bae0339494ecc919ce0689aedd49d
True Fantøm <116278271+TrueFantom@users.noreply.github.com>
2022-12-27 18:53:04 +0300

    Update network.js

--- network.js
+++ network.js
@@ -369,11 +369,11 @@
             items: [
               {
                 text: '(1) text',
-                value: 1
+                value: '1'
               },
               {
                 text: '(2) json',
-                value: 2
+                value: '2'
               }
             ]
           },
@@ -383,51 +383,51 @@
             items: [
               {
                 text: '(1) text',
-                value: 1
+                value: '1'
               },
               {
                 text: '(2) json',
-                value: 2
+                value: '2'
               },
               {
                 text: '(3) ok',
-                value: 3
+                value: '3'
               },
               {
                 text: '(4) url',
-                value: 4
+                value: '4'
               },
               {
                 text: '(5) type',
-                value: 5
+                value: '5'
               },
               {
                 text: '(6) headers',
-                value: 6
+                value: '6'
               },
               {
                 text: '(7) body',
-                value: 7
+                value: '7'
               },
               {
                 text: '(8) status',
-                value: 8
+                value: '8'
               },
               {
                 text: '(9) status text',
-                value: 9
+                value: '9'
               },
               {
                 text: '(10) body used',
-                value: 10
+                value: '10'
               },
               {
                 text: '(11) redirected',
-                value: 11
+                value: '11'
               },
               {
                 text: '(12) error',
-                value: 12
+                value: '12'
               }
             ]
           }
