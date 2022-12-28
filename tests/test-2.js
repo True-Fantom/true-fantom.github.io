37ff104f359564251d86014cf07d26144837dd7d
True Fantøm <116278271+TrueFantom@users.noreply.github.com>
2022-12-28 16:07:51 +0300

    Update test-2.js

--- tests/test-2.js
+++ tests/test-2.js
@@ -621,53 +621,65 @@
     }
 
     connected_to_internet_block() {
-      return navigator.onLine;
+      try {
+        return navigator.onLine;
+      } catch(err) {
+        return '';
+      }
     }
     
     browser_block() {
-      if (navigator.userAgent.includes('Firefox')) {
-        // 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0'
-        return 'Firefox';
-      } else if (navigator.userAgent.includes('SamsungBrowser')) {
-        // 'Mozilla/5.0 (Linux; Android 9; SAMSUNG SM-G955F Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/9.4 Chrome/67.0.3396.87 Mobile Safari/537.36'
-        return 'Samsung Internet';
-      } else if (((navigator.userAgent.includes('Opera') || navigator.userAgent.includes('OPR')) && navigator.userAgent.includes('GX')) || navigator.userAgent.includes('OPX')) {
-        // 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0 (Edition Yx GX)'
-        return 'Opera GX';
-      } else if (navigator.userAgent.includes('Opera') || navigator.userAgent.includes('OPR')) {
-        // 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 OPR/94.0.0.0 (Edition Yx 05)'
-        return 'Opera';
-      } else if (navigator.userAgent.includes('Trident')) {
-        // 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; Zoom 3.6.0; rv:11.0) like Gecko'
-        return 'Internet Explorer';
-      } else if (navigator.userAgent.includes('Edge')) {
-        // 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299'
-        return 'Edge Legacy';
-      } else if (navigator.userAgent.includes('Edg')) {
-        // 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36 Edg/104.0.1293.70'
-        return 'Edge Chromium';
-      } else if (navigator.userAgent.includes('YaBrowser') || navigator.userAgent.includes('YaSearchBrowser')) {
-        // 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 YaBrowser/22.11.5.715 Yowser/2.5 Safari/537.36'
-        return 'Yandex';
-      } else if (navigator.userAgent.includes('Miui')) {
-        // 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/534.24 XiaoMi/MiuiBrowser/13.19.2-gn'
-        return 'Mi Browser';
-      } else if (navigator.userAgent.includes('UBrowser')) {
-        // 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 UBrowser/6.0.1308.1016 Safari/537.36'
-        return 'UC Browser';
-      } else if (navigator.userAgent.includes('Chrome')) {
-        // 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
-        return 'Chromium';
-      } else if (navigator.userAgent.includes('Safari')) {
-        // 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Mobile/15E148 Safari/604.1'
-        return 'Safari';
-      } else {
+      try {
+        if (navigator.userAgent.includes('Firefox')) {
+          // 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0'
+          return 'Firefox';
+        } else if (navigator.userAgent.includes('SamsungBrowser')) {
+          // 'Mozilla/5.0 (Linux; Android 9; SAMSUNG SM-G955F Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/9.4 Chrome/67.0.3396.87 Mobile Safari/537.36'
+          return 'Samsung Internet';
+        } else if (((navigator.userAgent.includes('Opera') || navigator.userAgent.includes('OPR')) && navigator.userAgent.includes('GX')) || navigator.userAgent.includes('OPX')) {
+          // 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0 (Edition Yx GX)'
+          return 'Opera GX';
+        } else if (navigator.userAgent.includes('Opera') || navigator.userAgent.includes('OPR')) {
+          // 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 OPR/94.0.0.0 (Edition Yx 05)'
+          return 'Opera';
+        } else if (navigator.userAgent.includes('Trident')) {
+          // 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; Zoom 3.6.0; rv:11.0) like Gecko'
+          return 'Internet Explorer';
+        } else if (navigator.userAgent.includes('Edge')) {
+          // 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299'
+          return 'Edge Legacy';
+        } else if (navigator.userAgent.includes('Edg')) {
+          // 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36 Edg/104.0.1293.70'
+          return 'Edge Chromium';
+        } else if (navigator.userAgent.includes('YaBrowser') || navigator.userAgent.includes('YaSearchBrowser')) {
+          // 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 YaBrowser/22.11.5.715 Yowser/2.5 Safari/537.36'
+          return 'Yandex';
+        } else if (navigator.userAgent.includes('Miui')) {
+          // 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/534.24 XiaoMi/MiuiBrowser/13.19.2-gn'
+          return 'Mi Browser';
+        } else if (navigator.userAgent.includes('UBrowser')) {
+          // 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 UBrowser/6.0.1308.1016 Safari/537.36'
+          return 'UC Browser';
+        } else if (navigator.userAgent.includes('Chrome')) {
+          // 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+          return 'Chromium';
+        } else if (navigator.userAgent.includes('Safari')) {
+          // 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Mobile/15E148 Safari/604.1'
+          return 'Safari';
+        } else {
+          return '';
+        }
+      } catch(err) {
         return '';
       }
     }
     
     current_url_block() {
-      return document.URL;
+      try {
+        return document.URL;
+      } catch(err) {
+        return '';
+      }
     }
     
     network_type_block() {
