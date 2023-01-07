fd6f1d653f15f53e69e46ccbe8b40db6ea65a978
True Fantøm <116278271+TrueFantom@users.noreply.github.com>
2023-01-07 15:58:38 +0300

    Update test-3.js

--- tests/test-3.js
+++ tests/test-3.js
@@ -55,6 +55,27 @@
             blockType: Scratch.BlockType.COMMAND,
             text: '',
             arguments: {}
+          },
+          
+          {
+            opcode: 'a7_block',
+            blockType: Scratch.BlockType.COMMAND,
+            text: '',
+            arguments: {}
+          },
+          
+          {
+            opcode: 'a8_block',
+            blockType: Scratch.BlockType.COMMAND,
+            text: '',
+            arguments: {}
+          },
+          
+          {
+            opcode: 'a9_block',
+            blockType: Scratch.BlockType.COMMAND,
+            text: '',
+            arguments: {}
           }
           
         ],
@@ -80,32 +101,54 @@
     
     a3_block() {
       try {
-        let params = 'toolbar=0';
+        let params = 'menubar=1';
         window.open('https://extensions.turbowarp.org/', '_blank', params);
       } catch (err) {}
     }
     
     a4_block() {
       try {
-        let params = 'location=0';
+        let params = 'toolbar=0';
         window.open('https://extensions.turbowarp.org/', '_blank', params);
       } catch (err) {}
     }
     
     a5_block() {
       try {
-        let params = 'status=0';
+        let params = 'toolbar=1';
         window.open('https://extensions.turbowarp.org/', '_blank', params);
       } catch (err) {}
     }
     
     a6_block() {
       try {
-        window.open('https://extensions.turbowarp.org/', '_blank');
+        let params = 'location=0';
+        window.open('https://extensions.turbowarp.org/', '_blank', params);
       } catch (err) {}
     }
     
     a7_block() {
+      try {
+        let params = 'location=1';
+        window.open('https://extensions.turbowarp.org/', '_blank', params);
+      } catch (err) {}
+    }
+    
+    a8_block() {
+      try {
+        let params = 'status=0';
+        window.open('https://extensions.turbowarp.org/', '_blank', params);
+      } catch (err) {}
+    }
+    
+    a9_block() {
+      try {
+        let params = 'status=1';
+        window.open('https://extensions.turbowarp.org/', '_blank', params);
+      } catch (err) {}
+    }
+    
+    a0_block() {
       try {
         let params = 'width=100,height=100,left=0,top=0,menubar=1,toolbar=0,location=1,status=0';
         window.open('https://extensions.turbowarp.org/', '_blank', params);
