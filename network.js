8f0173aa03c550f2e386e207ee2b5130d6dd20d2
True Fantøm <116278271+True-Fantom@users.noreply.github.com>
2023-01-21 19:33:47 +0300

    Update network.js

--- network.js
+++ network.js
@@ -9,7 +9,7 @@
   const fetch_url = ({URL, BODY, CONTENT_TYPE, RESPONSES_TYPES, SPLIT}, METHOD) => {
     SPLIT = String(SPLIT);
     CONTENT_TYPE = Number(CONTENT_TYPE);
-    RESPONSES_TYPES = RESPONSES_TYPES.split(' ').filter(word => word !== '');
+    RESPONSES_TYPES = String(RESPONSES_TYPES).split(' ').filter(word => word !== '');
     let single = METHOD === 'GET' || METHOD === 'DELETE';
     return fetch(URL, {
       method: METHOD,
