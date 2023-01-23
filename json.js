ac72b9bdc82dcc3da5ecfe156f551f2c892430da
True Fantøm <116278271+True-Fantom@users.noreply.github.com>
2023-01-23 19:41:42 +0300

    Update json.js

--- json.js
+++ json.js
@@ -118,8 +118,8 @@
         if (path[path.length - 1] === '') {path.splice(-1, 1)}
         var json = JSON.parse(' ' + String(JSON_STRING));
         path.forEach(prop => json = json[prop]);
-        if (json === null) {return 'null'}
-        else if (json === undefined) {return ''}
+        if (json === null) {return null}
+        else if (json === undefined) {return undefined}
         else if (typeof json === 'object') {return JSON.stringify(json)}
         else {return String(json)}
       } catch(err) {return ''}
