49f9c871f3e1c80edf9c2005e31b804383cc5cab
True Fantøm <116278271+True-Fantom@users.noreply.github.com>
2023-02-12 14:21:14 +0300

    Update network.js

--- network.js
+++ network.js
@@ -27,7 +27,7 @@
 
   const FetchUrl = ({USER_URL, BODY, CONTENT_TYPE, RESPONSES_TYPES}, METHOD) => {
     CONTENT_TYPE = Number(CONTENT_TYPE);
-    RESPONSES_TYPES = Array.isArray(JsonObj(RESPONSES_TYPES)) ? JsonObj(RESPONSES_TYPES) : Array.from({length: 1}, (v) => RESPONSES_TYPES);
+    RESPONSES_TYPES = Array.isArray(JsonObj(RESPONSES_TYPES)) ? JsonObj(RESPONSES_TYPES).length === 0 ? [9] : JsonObj(RESPONSES_TYPES) : Array.from({length: 1}, (v) => RESPONSES_TYPES);
     const single = METHOD === 'GET' || METHOD === 'DELETE';
     return fetch(String(USER_URL), {
       method: METHOD,
