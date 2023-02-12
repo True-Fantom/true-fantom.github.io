8a473b64b46b9ac235b79a355c7fa14bfceef704
True Fantøm <116278271+True-Fantom@users.noreply.github.com>
2023-02-12 21:29:42 +0300

    Update json.js

--- json.js
+++ json.js
@@ -229,7 +229,7 @@
       try {
         JSON_STRING = JsonObj(JSON_STRING, true);
         PATH = Array.isArray(JsonObj(PATH)) ? JsonObj(PATH) : Array.from({length: 1}, (v) => PATH);
-        PATH.forEach(prop => JSON_STRING = JSON_STRING[String(prop)]);
+        if PATH !== '' {PATH.forEach(prop => JSON_STRING = JSON_STRING[String(prop)])}
         if (typeof JSON_STRING === 'object') {return JsonStr(JSON_STRING)}
         else if (JSON_STRING === undefined) {return ''}
         else {return JSON_STRING}
