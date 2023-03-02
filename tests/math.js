0ffa4d58228eda5bf10f028d578d718dde1be712
True Fantøm <116278271+True-Fantom@users.noreply.github.com>
2023-03-02 18:23:06 +0300

    Update math.js

--- tests/math.js
+++ tests/math.js
@@ -22,7 +22,7 @@
 
   const Cast = Scratch.Cast;
 
-  class ScratchMath {
+  class Extension {
 
     getInfo() {
       return {
@@ -361,5 +361,5 @@
     }
   }
 
-  Scratch.extensions.register(new ScratchMath());
+  Scratch.extensions.register(new Extension());
 })(Scratch);
