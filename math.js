a171b08b44f842de70c6c78421ff197db3e1325f
True Fantøm <116278271+True-Fantom@users.noreply.github.com>
2023-03-05 14:45:06 +0300

    Update math.js

--- math.js
+++ math.js
@@ -52,37 +52,6 @@
     return n1 - n2;
   };
 
-  const almostCompare = (v1, v2) => {
-    let n1 = Number(v1);
-    let n2 = Number(v2);
-    if (n1 === 0 && isNotActuallyZero(v1)) {
-      n1 = NaN;
-    } else if (n2 === 0 && isNotActuallyZero(v2)) {
-      n2 = NaN;
-    }
-    if (isNaN(n1) || isNaN(n2)) {
-      // At least one argument can't be converted to a number.
-      // Scratch compares strings as case insensitive.
-      const s1 = String(v1).toLowerCase();
-      const s2 = String(v2).toLowerCase();
-      if (s1 < s2) {
-        return -1;
-      } else if (s1 > s2) {
-        return 1;
-      }
-      return 0;
-    }
-    // Handle the special case of Infinity
-    if (
-      (n1 === Infinity && n2 === Infinity) ||
-      (n1 === -Infinity && n2 === -Infinity)
-    ) {
-      return 0;
-    }
-    // Compare as numbers, but it almost here
-    return Math.round(n1) - Math.round(n2);
-  };
-
   const toNaNNumber = value => {
     // If value is already a number we don't need to coerce it with
     // Number().
@@ -499,10 +468,12 @@
       return exactlyCompare(A, B) !== 0;
     }
     almost_equal_block({A, B}) {
-      return almostCompare(A, B) === 0;
+      const c = cast.compare(A, B);
+      return c <= 0.5 && c >= -0.5;
     }
     not_almost_equal_block({A, B}) {
-      return almostCompare(A, B) !== 0;
+      const c = cast.compare(A, B);
+      return !(c <= 0.5 && c >= -0.5);
     }
     nand_block({A, B}) {
       return !(cast.toBoolean(A) && cast.toBoolean(B));
