d6346ba1f423c9d564ec3103f071c3c1ada7c516
True Fantøm <116278271+True-Fantom@users.noreply.github.com>
2023-03-26 00:42:05 +0300

    Update math.js

--- math.js
+++ math.js
@@ -187,7 +187,7 @@
             arguments: {
               A: {
                 type: Scratch.ArgumentType.NUMBER,
-                defaultValue:
+                defaultValue: undefined
               }
             }
           },
