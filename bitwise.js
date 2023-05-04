c1b4dd49e064c8cfdce7b03a63cd7bf8e192a3f0
True Fantøm <116278271+True-Fantom@users.noreply.github.com>
2023-05-04 11:36:04 +0300

    Update bitwise.js

--- bitwise.js
+++ bitwise.js
@@ -237,7 +237,7 @@
     }
     bitwiseCircularLeftShift({LEFT, RIGHT}) {
       let arr = Array.from(number2bits(LEFT));
-      for (let i = 0; i < cast.toNumber(RIGHT) % 32; i++) {arr.push(arr.shift())}
+      for (let i = 0; i < number2bits(RIGHT).substring(29,33); i++) {arr.push(arr.shift())}
       return bits2number(arr.join(''));
     }
     bitwiseAnd({LEFT, RIGHT}) {
