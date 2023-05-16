6b5697d2702fdf7860156df9f6681071f201c8a6
True Fantøm <116278271+True-Fantom@users.noreply.github.com>
2023-05-16 09:03:48 +0300

    Update base.js

--- base.js
+++ base.js
@@ -64,10 +64,10 @@
     }
 
     is_base_block({A, B}) {
-      return new RegExp('^[' + chars.substring(0, cast.toNumber(B) - 1) + ']+$').test(cast.toString(A));
+      return new RegExp('^[' + chars.substring(0, cast.toNumber(B)) + ']+$').test(cast.toString(A));
     }
     base_block({A, B, C}) {
-      if (new RegExp('^[' + chars.substring(0, cast.toNumber(B) - 1) + ']+$').test(cast.toString(A))) {
+      if (new RegExp('^[' + chars.substring(0, cast.toNumber(B)) + ']+$').test(cast.toString(A))) {
         return parseInt(cast.toString(A), cast.toNumber(B)).toString(cast.toNumber(C));
       }
       return 0;
