1d5585c0eb415ac0a3e1c26acb984f5a0a88149b
True Fantøm <116278271+True-Fantom@users.noreply.github.com>
2023-05-19 09:08:28 +0300

    Create translate.js

---
+++ translate.js
@@ -0,0 +1,41 @@
+(function(Scratch) {
+
+  'use strict';
+
+  Scratch.translate.setup({
+
+    en: {
+
+      test: 'EN message'
+
+    },
+
+    es: {
+
+      test: 'ES message'
+
+    }
+
+  });
+
+  class Test {
+
+    getInfo() {
+
+      return {
+
+        id: 'testl10n',
+
+        name: Scratch.translate('Default message'),
+
+        blocks: []
+
+      };
+
+    }
+
+  }
+
+  Scratch.extensions.register(new Test());
+
+})(Scratch);
