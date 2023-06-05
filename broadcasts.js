d21c2ce6715f33d91815a19d50d8235be1d0c475
True Fantøm <116278271+True-Fantom@users.noreply.github.com>
2023-06-06 01:01:49 +0300

    Update broadcasts.js

--- broadcasts.js
+++ broadcasts.js
@@ -1,1 +1,40 @@
+(Scratch => {
+  'use strict';
 
+  class ScratchTest {
+    getInfo() {
+      return {
+
+        id: 'truefantombroadcasts',
+        name: 'Broadcasts',
+        color1: '#e08b9f',
+
+        blocks: [
+          {
+            opcode: 'broadcast_block',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'broadcast [MENU]',
+            arguments: {
+              MENU: {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'broadcasts_menu'
+              }
+            }
+          }
+        ],
+        menus: {
+          broadcasts_menu: {
+            acceptReporters: true,
+            items: ['test1', 'test2']
+          }
+        }
+      };
+    }
+
+    broadcast_block() {
+      return true;
+    }
+  }
+
+  Scratch.extensions.register(new ScratchTest());
+})(Scratch);
